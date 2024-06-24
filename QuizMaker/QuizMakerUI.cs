9fcc1fac4274caf9c182b286f7e3de2a501cd636
ugochukwumaduakor <umaduakor@hotmail.com>
2024-06-25 02:03:32 +1000

    create program menu

---
+++ QuizMaker/QuizMakerUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace QuizMaker
+{
+	public class QuizMakerUI
+	{
+		public static void  DiplayProgramMenu()
+		{
+            Console.WriteLine("************************ Welcome to Quiz Maker *************************\n");
+            Console.WriteLine("Menu:\n" +
+                "1. Admin Login\n" +
+                "2. User Login\n" +
+                "3. View Answers to quiz\n" +
+                "4. User Score Records\n\n" +
+                "Please choose the number of the page you want to visit... ");
+        }
+	}
+}
+
